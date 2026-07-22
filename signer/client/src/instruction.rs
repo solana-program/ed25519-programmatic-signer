7b14b8674dbbb6a3f1a81d3e2cfe3820aa379a0f
Gabe Rodriguez <grod220@gmail.com>
2026-07-22 04:20:25 -0400

    no-std is_writable fn

--- signer/client/src/instruction.rs
+++ signer/client/src/instruction.rs
@@ -1,8 +1,8 @@
 //! Builder for the on-chain `Submit` instruction.
 
 use {
+    alloc::collections::BTreeSet,
     solana_instruction::{AccountMeta, Instruction},
-    solana_message::VersionedMessage,
     solana_transaction::versioned::VersionedTransaction,
     spl_ed25519_signer_interface::instruction::Instruction as SignerInstruction,
 };
@@ -17,7 +17,9 @@ pub fn submit(transaction: VersionedTransaction) -> Instruction {
         .map(|(index, key)| AccountMeta {
             pubkey: *key,
             is_signer: false,
-            is_writable: is_message_account_writable(index, &transaction.message),
+            is_writable: transaction
+                .message
+                .is_maybe_writable_with_reserved_addresses(index, None::<&BTreeSet<_>>),
         })
         .collect();
     Instruction::new_with_wincode(
@@ -25,23 +27,4 @@ pub fn submit(transaction: VersionedTransaction) -> Instruction {
         &SignerInstruction::Submit(transaction),
         accounts,
     )
-}
-
-// TODO: Replace when no-std version of: https://github.com/anza-xyz/solana-sdk/blob/042f3451979cc8e31a45a09a5627a387ac12a067/message/src/lib.rs#L155-L235
-fn is_message_account_writable(index: usize, message: &VersionedMessage) -> bool {
-    // [writable signers | readonly signers | writable unsigned | readonly unsigned]
-    let header = message.header();
-    let account_keys = message.static_account_keys();
-    let required_signatures = usize::from(header.num_required_signatures);
-    let writable_signers_end =
-        required_signatures.saturating_sub(usize::from(header.num_readonly_signed_accounts));
-    let writable_unsigned_end = account_keys
-        .len()
-        .saturating_sub(usize::from(header.num_readonly_unsigned_accounts));
-    let is_writable_index = index < writable_signers_end
-        || (required_signatures..writable_unsigned_end).contains(&index);
-
-    is_writable_index
-        && (!message.is_invoked(index)
-            || account_keys.contains(&solana_sdk_ids::bpf_loader_upgradeable::id()))
 }
